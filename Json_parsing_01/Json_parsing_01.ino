422555a9bec1bcc990ca2a55049cb88e5e603a4a
CoolDong <32934089+Lcooldong@users.noreply.github.com>
2021-09-13 21:53:59 +0900

    practice_parsing

---
+++ Json_parsing_01/Json_parsing_01.ino
@@ -0,0 +1,32 @@
+#include <ArduinoJson.h>
+#define LED 5
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available()){
+    //line feed (\n)까지 받음(보내는 쪽에서 \n 안붙이면 default setTimeout() 1초)
+    
+    String data = Serial.readStringUntil(0x0a);
+    Serial.println(data);
+    StaticJsonDocument<48> doc;
+    deserializeJson(doc, data);
+    String led_num = doc["num"];
+    String led_set = doc["set"]; 
+
+    Serial.println(led_num + "," + led_set);
+    if(led_num == "1"){
+      if(led_set == "on")
+      {
+          digitalWrite(LED, HIGH);
+      }else if(led_set == "off")
+      {
+        digitalWrite(LED, LOW);  
+      }
+    }
+
+  }
+}
---
+++ Json_parsing_02/Json_parsing_02.ino
@@ -0,0 +1,16 @@
+#define button1 23
+#define button2 22
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+
+}
+
+void loop() {
+  bool btn1 = digitalRead(button1);
+  bool btn2 = digitalRead(button2);
+  Serial.println("{\"btn1\": \""+String(btn1)+"\",\"btn2\": \""+String(btn2)+"\"}");
+  delay(500);
+}
